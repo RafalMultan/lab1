a168bfaffb559470457345b82fbec9bd1aaebb15
Rafal Multan <226169@student.pwr.edu.pl>
2016-03-18 18:51:49 +0100

    4

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//
+
 char **charSquare(int n);
 void drawCharSquare(char **square, int n);
 
@@ -9,8 +9,9 @@ int main(int argc, char **argv)
 {
 	int n;
 	n=atoi(argv[1]);
-	
-	drawCharSquare(charSquare(n), n);	
+	srand(atoi(argv[2]));
+	drawCharSquare(charSquare(n), n);
+	printf("%d \n",atoi(argv[2]));	
 }
 
 char **charSquare(int n)
@@ -21,7 +22,7 @@ char **charSquare(int n)
 	{
 		square[i]=new char[n];
 		for(int j=0;j<n;j++)
-			square[i][j]=33+rand()%(126-33+1);
+			square[i][j]=97+rand()%(122-97+1);
 	}
 	return square;
 }
