e2789d7ac9462b4a05df11765d2c6a1ecff65e76
Rafal Multan <226169@student.pwr.edu.pl>
2016-03-18 18:12:15 +0100

    nie dziala

--- main.cpp
+++ main.cpp
@@ -8,16 +8,32 @@ void drawCharSquare(char **square, int n);
 int main(int argc, char **argv)
 {
 	int n;
+	n=atoi(argv[1]);
+	printf("%d",n);
 	drawCharSquare(charSquare(n), n);	
 }
 
 char **charSquare(int n)
 {
 	char **square;
+	square=new char *[n];
+	for(int i=0;i<n;i++)
+	{
+		square[i]=new char[n];
+		for(int j=0;j<n;j++)
+			square[i][j]=33+rand()%(126-33+1);
+	}
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
+	for(int i=0;i<n;i++)
+		
+		{
+			for(int j=0;j<n;j++)
+			printf("%c ",square[i][j]);
+		printf("\n");
+		}
 
 }
